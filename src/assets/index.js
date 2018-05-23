6b0815c5d19b304aed6a8c3286cc1730b41f9321
Sindri Guðmundsson <sindrig@tempo.io>
2018-05-23 00:03:23 +0000

    Player assets added. need more.
    Former-commit-id: 9563b529e501fb25cbe8ea693402984d242a5537

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,10 +1,15 @@
 const requireAssets = require.context('./assets', true, /.*/);
+const requirePlayers = require.context('./players', true, /.*/);
 
 const RE = /^[\\./\\]+/;
+const getKey = (key, loc) => `${loc}/${key.replace(RE, '')}`;
 
 const exp = {};
 requireAssets.keys().forEach((key) => {
-    exp[key.replace(RE, '')] = requireAssets(key);
+    exp[getKey(key, 'assets')] = requireAssets(key);
+});
+requirePlayers.keys().forEach((key) => {
+    exp[getKey(key, 'players')] = requirePlayers(key);
 });
 
 module.exports = exp;
---
+++ src/constants.js
@@ -0,0 +1,9 @@
+export const VP = {
+    height: 176,
+    width: 240,
+};
+
+export const THUMB_VP = {
+    height: 50,
+    width: 100,
+};
