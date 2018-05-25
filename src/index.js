76ee1df9db1748da83146f027a9f968f1566444a
Sindri Guðmundsson <sindrig@tempo.io>
2018-05-25 17:19:52 +0000

    get rid of service worker for now

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// TODO maybe use SSL and get service worker...?
+// import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// registerServiceWorker();
