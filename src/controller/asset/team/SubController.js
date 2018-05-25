7e01baf1bf79155aca624cbc7f2880288360e53a
Sindri Guðmundsson <sindrig@tempo.io>
2018-05-25 16:49:03 +0000

    Limit sub to same team
    Former-commit-id: 6ea601837d7511f5ba867aee51ca91590bb1a41c

--- src/controller/asset/team/SubController.js
+++ src/controller/asset/team/SubController.js
@@ -2,8 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { playerPropType } from '../../../propTypes';
 
-const SubController = ({ subIn, subOut, addSubAsset }) => (
+const SubController = ({ subTeam, subIn, subOut, addSubAsset }) => (
     <div>
+        {subTeam}
         {subIn && <div className="substition-player">Út: {`#${subIn.number} - ${subIn.name}`}</div>}
         {subOut && <div className="substition-player">Inn: {`#${subOut.number} - ${subOut.name}`}</div>}
         {subIn && subOut && <button onClick={addSubAsset}>Setja í röð</button>}
@@ -13,12 +14,14 @@ const SubController = ({ subIn, subOut, addSubAsset }) => (
 SubController.propTypes = {
     subIn: playerPropType,
     subOut: playerPropType,
+    subTeam: PropTypes.string,
     addSubAsset: PropTypes.func.isRequired,
 };
 
 SubController.defaultProps = {
     subIn: null,
     subOut: null,
+    subTeam: null,
 };
 
 export default SubController;
