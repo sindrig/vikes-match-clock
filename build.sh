77495f89dd4b3935a4837beeb39453921e68ebdb
Sindri Guðmundsson <sindrigudmundsson@gmail.com>
2019-10-23 12:30:27 +0000

    cleanup

--- build.sh
+++ build.sh
@@ -1,25 +1,5 @@
-EXTRA_S3=""
-if [ -z $PROFILE ]; then
-    EXTRA_S3="--profile=$PROFILE"
-fi
-build() {
-    npm run build
-}
-
-check-master() {
-    if [ -z ${LAMBCI_BRANCH+x} ]; then
-        LAMBCI_BRANCH=$(git rev-parse --abbrev-ref HEAD)
-    fi
-    [[ "$LAMBCI_BRANCH" == "master" ]] || { echo >&2 "Not on master ($LAMBCI_BRANCH)"; return 1; }
-}
-
-node-modules() {
-    npm install
-}
-
-deploy() {
-    node_modules/.bin/s3-deploy "./build/**" --cwd "./build/" --region "eu-west-1" --bucket klukka.irdn.is --gzip
-}
+#!/usr/bin/env bash
+set -e
 
 windows() {
     ./package.sh http://klukka.irdn.is --platform windows
@@ -31,72 +11,23 @@ platform() {
     ./package.sh http://klukka.irdn.is
 }
 
-lint() {
-    npm run lint
-}
-
-test() {
-    npm run test
-}
-
-all() {
-    if check-master; then
-        main node-modules lint test build deploy
-        EXIT_CODE=$?
-    else
-        main node-modules lint test
-        EXIT_CODE=$?
-    fi
-    exit $EXIT_CODE
-}
-
 main() {
     for ACTION in "$@"
     do
         echo "Running $ACTION"
         case $ACTION in
-            build)
-                build
-                ;;
-            check-master)
-                check-master;
-                ;;
-            node-modules)
-                node-modules;
-                ;;
-            deploy)
-                deploy;
-                ;;
             windows)
                 windows;
                 ;;
             platform)
                 platform;
                 ;;
-            lint)
-                lint;
-                ;;
-            all)
-                all;
-                ;;
-            test)
-                test;
-                ;;
             *)
                 echo "Unknown action $ACTION"
                 exit 1
                 ;;
         esac
-        EXIT_CODE=$?
-        if [ $EXIT_CODE -ne 0 ]; then
-            echo "Exiting $ACTION with $EXIT_CODE"
-            exit $EXIT_CODE
-        fi
     done
-    exit $?
 }
 
 main "$@"
-EXIT_CODE=$?
-echo "Exiting with exit code $EXIT_CODE"
-exit $EXIT_CODE
