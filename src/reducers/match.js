d150eb522f190b4abb631d22fe72ab5dd794def1
Sindri Guðmundsson <sindrigudmundsson@gmail.com>
2020-01-25 13:06:52 +0000

    Trust state started if we had already started

--- src/reducers/match.js
+++ src/reducers/match.js
@@ -161,18 +161,30 @@ const actions = {
         next(state, { data, path }) {
             if (path === 'match' && data) {
                 const results = { ...state, ...data };
-                if (results.started > 0 && state.started === 0) {
-                    // We just pressed start clock. Trust our own time.
-                    // Compensate for some small lag
-                    results.started = Date.now() - 200;
+                if (results.started > 0) {
+                    if (state.started === 0) {
+                        // We just pressed start clock. Trust our own time.
+                        // Compensate for some small lag
+                        results.started = Date.now() - 150;
+                    } else {
+                        results.started = state.started;
+                    }
                 }
-                if (results.timeout > 0 && state.timeout === 0) {
-                    // We just pressed start clock. Trust our own time.
-                    // Compensate for some small lag
-                    results.timeout = Date.now() - 200;
+                if (results.timeout > 0) {
+                    if (state.timeout === 0) {
+                        // We just pressed start timeout. Trust our own time.
+                        // Compensate for some small lag
+                        results.timeout = Date.now() - 150;
+                    } else {
+                        results.timeout = state.timeout;
+                    }
                 }
-                if (results.buzzer && !state.buzzer) {
-                    results.buzzer = Date.now();
+                if (results.buzzer) {
+                    if (!state.buzzer) {
+                        results.buzzer = Date.now();
+                    } else {
+                        results.buzzer = state.buzzer;
+                    }
                 }
                 if (!data.home2min) {
                     results.home2min = [];
