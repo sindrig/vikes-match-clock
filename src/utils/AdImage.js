7f094a576135413f80cef405a76ba440283ac3ec
Sindri Guðmundsson <sindrigudmundsson@gmail.com>
2020-06-09 16:09:43 +0000

    Add construct for ads

--- src/utils/AdImage.js
+++ src/utils/AdImage.js
@@ -1,6 +1,5 @@
-// import React from 'react';
+import React from 'react';
 
-// import adImage from '../images/borgun.png';
+import adImage from '../images/borgun.png';
 
-export default () => null;
-// export default () => <img src={adImage} className="ad" alt="Ad" />;
+export default () => <img src={adImage} className="ad" alt="Ad" />;
