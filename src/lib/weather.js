7b7047eeee923f770ccb227bcd0494c7a6e6a04e
Sindri Guðmundsson <sindrigudmundsson@gmail.com>
2022-12-06 09:45:43 +0000

    Use weather station closer to Vikin

--- src/lib/weather.js
+++ src/lib/weather.js
@@ -3,7 +3,7 @@ import apiConfig from "../apiConfig";
 
 export const getTemp = () =>
   axios.get(`${apiConfig.gateWayUrl}currentWeather`).then(({ data }) => {
-    const temperature = Math.ceil(parseFloat(data?.main?.temp_max));
+    const temperature = Math.ceil(data?.temp);
     if (!Number.isNaN(temperature)) {
       return temperature;
     }
