6a02d2a26771f00409e77debe8a3e6c1ddef800a
Sindri Guðmundsson <sindrigudmundsson@gmail.com>
2024-04-29 01:09:08 +0000

    Create folders to manage images

--- src/controller/media/MediaManager.js
+++ src/controller/media/MediaManager.js
@@ -8,9 +8,23 @@ import { IMAGE_TYPES } from ".";
 
 const MediaManager = ({ auth }) => {
   const [tab, setTab] = useState(IMAGE_TYPES.images);
+  const [prefix, setPrefix] = useState("");
+  const [ts, setTs] = useState(null);
   const [displayNow, setDisplayNow] = useState(true);
   const selectTab = (tab) => {
+    setPrefix("");
     setTab(tab);
+  };
+  const appendPrefix = (newPrefix) => {
+    if (newPrefix === "..") {
+      setPrefix(prefix.slice(0, prefix.lastIndexOf("/")));
+    } else {
+      setPrefix(`${prefix}/${newPrefix}`);
+    }
+  };
+  const refresh = () => {
+    // How stupid is this? sorry
+    [2, 3, 5, 6].forEach((i) => setTimeout(() => setTs(Date.now()), 500 * i));
   };
   return (
     <div className="control-item withborder">
@@ -20,7 +34,9 @@ const MediaManager = ({ auth }) => {
         <Nav.Item eventKey={IMAGE_TYPES.smallAds}>Augl (litlar)</Nav.Item>
         <Nav.Item eventKey={IMAGE_TYPES.players}>Leikmenn</Nav.Item>
       </Nav>
-      {!auth.isEmpty && <UploadManager prefix={tab} />}
+      {!auth.isEmpty && (
+        <UploadManager prefix={`${tab}${prefix}`} refresh={refresh} />
+      )}
       <div>
         <label>
           Birta strax
@@ -40,7 +56,13 @@ const MediaManager = ({ auth }) => {
           />
         </label>
       </div>
-      <ImageList prefix={tab} displayNow={displayNow} />
+      <ImageList
+        prefix={`${tab}${prefix}`}
+        appendPrefix={appendPrefix}
+        displayNow={displayNow}
+        allowEdit={!auth.isEmpty}
+        ts={ts}
+      />
     </div>
   );
 };
