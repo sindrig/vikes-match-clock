62c20654eb8a15da3d4b6bb79c757343a1db7bdd
Sindri Guðmundsson <sindrigudmundsson@gmail.com>
2026-02-17 13:41:14 +0000

    fix(clock): tighten TeamController team prop type and use crypto.randomUUID()

--- clock/src/match-controller/TeamController.tsx
+++ clock/src/match-controller/TeamController.tsx
@@ -5,36 +5,28 @@ import { PENALTY_LENGTH } from "../constants";
 import ControlButton from "./ControlButton";
 
 interface TeamControllerProps {
-  team: string;
+  team: "home" | "away";
   started: number | null;
 }
 
-const uuidv4 = (): string =>
-  "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, (c) => {
-    const r = (Math.random() * 16) | 0;
-    const v = c === "x" ? r : (r & 0x3) | 0x8;
-    return v.toString(16);
-  });
-
 const TeamController: React.FC<TeamControllerProps> = ({ team, started }) => {
   const { addGoal, addPenalty, matchTimeout } = useMatch();
-  const teamKey = team as "home" | "away";
 
   return (
     <div
       className={`match-controller-box match-controller-box-${String(team)}`}
     >
-      <ControlButton className="yellow" onClick={() => addGoal(teamKey)}>
+      <ControlButton className="yellow" onClick={() => addGoal(team)}>
         Mark
       </ControlButton>
       <ControlButton
         className="red"
-        onClick={() => addPenalty(teamKey, uuidv4(), PENALTY_LENGTH)}
+        onClick={() => addPenalty(team, crypto.randomUUID(), PENALTY_LENGTH)}
         disabled={!!started}
       >
         Brottvísun
       </ControlButton>
-      <ControlButton className="green" onClick={() => matchTimeout(teamKey)}>
+      <ControlButton className="green" onClick={() => matchTimeout(team)}>
         Leikhlé
       </ControlButton>
     </div>
