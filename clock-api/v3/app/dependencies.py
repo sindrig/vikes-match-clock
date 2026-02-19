bfe173753b1c807f7af25796a6440ef4b4e7e8d2
Sindri Guðmundsson <sindrigudmundsson@gmail.com>
2026-02-19 21:58:40 +0000

    throw

--- clock-api/v3/app/dependencies.py
+++ clock-api/v3/app/dependencies.py
@@ -2,6 +2,7 @@ import boto3
 from functools import cache
 
 from app.services.ksi import KsiClient
+from fastapi.exceptions import HTTPException
 
 ssm_client = boto3.client("ssm", region_name="eu-west-1")
 
@@ -11,7 +12,12 @@ def get_ksi_api_key(team_id: int) -> str:
     response = ssm_client.get_parameter(
         Name=f"/vikes-match-clock/ksi-api-key/{team_id}"
     )
-    return response["Parameter"]["Value"]
+    try:
+        return response["Parameter"]["Value"]
+    except KeyError:
+        raise HTTPException(
+            status_code=500, detail="KSI API key not found for team_id: {team_id}"
+        )
 
 
 @cache
